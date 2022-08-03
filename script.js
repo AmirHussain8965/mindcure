3822410e691d21f1d5f0469cd580e80029f9d234
Amir Hussain <92318161+AmirHussain8965@users.noreply.github.com>
2022-08-02 21:29:26 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+//onscroll = function() {
+//    var scroll = window.pageYOffset || document.documentElement.scrollTop;
+//    if (scroll > 0) {
+//        document.getElementById("header").className = "header-scroll";
+//    } else {
+
+//        document.getElementById("header").className = "header";
